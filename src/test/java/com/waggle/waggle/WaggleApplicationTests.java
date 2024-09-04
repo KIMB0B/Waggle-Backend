49806d3e25a8df0c37117605aa826e50269be935
KIMB0B <axzsd96301@gmail.com>
2024-09-05 01:27:33 +0900

    initial commit

---
+++ src/test/java/com/waggle/waggle/WaggleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waggle.waggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaggleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
