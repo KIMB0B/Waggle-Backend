25af572ced07ae63cac42254280015a6c8f11790
KIMB0B <axzsd96301@gmail.com>
2024-09-05 03:19:03 +0900

    feat: Spring Security와 OAuth2 설정

---
+++ src/main/java/com/waggle/waggle/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.waggle.waggle.config;
+
+import com.waggle.waggle.auth.OAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final OAuth2UserService oAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(authorize -> {
+            authorize
+                    .requestMatchers("/user/**").authenticated()
+                    .requestMatchers("/admin/**").hasRole("ADMIN")
+                    .anyRequest().permitAll();
+        });
+
+        http.oauth2Login(oauth2Configurer -> {
+            oauth2Configurer.
+                    loginPage("/login")
+                    .userInfoEndpoint(userInfoEndpointConfig -> {
+                        userInfoEndpointConfig.userService(oAuth2UserService);
+                    });
+        });
+
+        return http.build();
+    }
+}
