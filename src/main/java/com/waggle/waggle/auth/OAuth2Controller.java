4a3b10ddc5bcc6b02109f483162f83c43c4fd017
KIMB0B <axzsd96301@gmail.com>
2024-09-07 03:10:51 +0900

    feat: Kakao 로그인에 사용자 정보 불러오는 기능 추가

--- src/main/java/com/waggle/waggle/auth/OAuth2Controller.java
+++ src/main/java/com/waggle/waggle/auth/OAuth2Controller.java
@@ -1,14 +1,16 @@
 package com.waggle.waggle.auth;
 
+import com.waggle.waggle.auth.dto.KakaoTokenResponseDto;
 import com.waggle.waggle.auth.service.KakaoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/oauth2")
 public class OAuth2Controller {
@@ -16,9 +18,11 @@ public class OAuth2Controller {
     @Autowired
     private KakaoService kakaoService;
 
-    @GetMapping("/code/kakao")
-    public ResponseEntity<?> callback(@RequestParam("code") String code) {
-        String accessToken = kakaoService.getAccessTokenFromKakao(code);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping("/kakao/code")
+    public ResponseEntity<?> loginKakao(@RequestParam("code") String code) {
+        KakaoTokenResponseDto response = kakaoService.getAccessToken(code);
+        String userInfo = kakaoService.getUserInfo(response.accessToken);
+        log.info("사용자 정보: {}", userInfo);
+        return ResponseEntity.ok(response);
     }
 }
